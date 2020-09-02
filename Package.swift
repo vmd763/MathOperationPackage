b066dd78a579cc9845938cecafdcd91fc31ae49b
vmd763 <vmd763@zebra.com>
2020-09-02 13:17:25 +0530

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MathOperation",
-            dependencies: ["MathOperations"]),
+            dependencies: ["MathOperation"]),
         .testTarget(
             name: "MathOperationPackageTests",
             dependencies: ["MathOperationPackage"]),
